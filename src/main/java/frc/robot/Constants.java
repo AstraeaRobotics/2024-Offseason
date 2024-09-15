6291b9079cd5966081baaa0161386508a96cbe77
kushashar-4 <110451154+kushashar-4@users.noreply.github.com>
2024-09-15 09:30:33 -0400

    Logic Fixes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -41,8 +41,8 @@ public final class Constants {
     public static final double driveKD = 0;
 
     // FeedForward Constants
-    public static final double turnKV = 0.00005; // 0.31
-    public static final double driveKV = 0.25; // 0.45
+    // public static final double turnKV = 0.00005; // 0.31
+    public static final double driveKV = 0.65; // 0.45
 
   }
 
