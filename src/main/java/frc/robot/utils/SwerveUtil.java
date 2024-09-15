a7ae5dd4565c04ea64f79347a25e47d8bf63791e
kushashar-4 <110451154+kushashar-4@users.noreply.github.com>
2024-09-15 09:31:38 -0400

    Logic Fixes

--- src/main/java/frc/robot/utils/SwerveUtil.java
+++ src/main/java/frc/robot/utils/SwerveUtil.java
@@ -8,6 +8,8 @@ public class SwerveUtil {
     public static double[] optimizeModule(double currentAngle, double desiredAngle, double speed) {
         double[] optimizedModule = new double[2];
         double angleDifference = desiredAngle - currentAngle;
+        optimizedModule[0] = desiredAngle;
+        optimizedModule[1] = speed;
         
         // Optimizes the desired angle so the module never turns more than 90 degrees
         if(Math.abs(angleDifference) > 90 && Math.abs(angleDifference) < 270) {
@@ -16,7 +18,7 @@ public class SwerveUtil {
         }
 
         // Drive speed of module proportional to its closeness to desired angle to prevent slippage
-        optimizedModule[1] *= Math.cos(Math.abs(desiredAngle - optimizedModule[0]));
+        optimizedModule[1] *= Math.abs(Math.cos(currentAngle - optimizedModule[0]));
 
         return optimizedModule;
     }
